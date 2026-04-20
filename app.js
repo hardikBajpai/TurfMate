e7b041e4deab329f66656b19c88fee8685f1c754
hardikBajpai <hardikbajpai22@gmail.com>
2026-04-20 21:32:58 +0530

    Initialize User Schema

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/turfmate');
+
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
