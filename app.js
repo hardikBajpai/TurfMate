65a85164e5ad75857c624bdbef6e9ec6b2cba2b2
hardikBajpai <hardikbajpai22@gmail.com>
2026-06-27 16:22:38 +0530

    Test Deployment

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 app.get('/' , (req,res)=>{
-  res.render("/turfs/home.ejs");
+  res.send("Server is running!!");
 })
 
 const store = MongoStore.create({
